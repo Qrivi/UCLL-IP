a96cdd676b4e6ba15467f06f4909ce0ddd001673
Kristof Dewilde <me@krivi.be>
2016-03-30 23:37:43 +0200

    Removed old tests not compatible with refactored model

--- domain/src/test/java/be/krivi/ucll/ip/domain/LocationTest.java
+++ domain/src/test/java/be/krivi/ucll/ip/domain/LocationTest.java
@@ -1,8 +1,6 @@
 package be.krivi.ucll.ip.domain;
 
-import be.krivi.ucll.ip.domain.exception.DomainException;
 import be.krivi.ucll.ip.domain.core.Location;
-import org.junit.Test;
 
 /**
  * Created by Krivi on 21/02/16.
@@ -11,31 +9,31 @@ public class LocationTest{
 
     Location location;
 
-    @Test
-    public void test_create_location_with_standard_address() throws Exception{
-        location = new Location(
-                "Bij Koning Filip", // name
-                50.885933, // latitude
-                4.354272, // longitude
-                "Avenue du Parc Royal 1", // address
-                "", // cross street
-                "Laeken", // city
-                1020, // zip code
-                "Belgium" // coountry
-        );
-    }
-
-    @Test( expected = DomainException.class )
-    public void test_fail_create_location_with_non_existent_coordinates() throws Exception{
-        location = new Location(
-                "Bij Koning Filip", // name
-                150.885933, // latitude
-                404.354272, // longitude
-                "Avenue du Parc Royal 1", // address
-                "", // cross street
-                "Laeken", // city
-                1020, // zip code
-                "Belgium" // coountry
-        );
-    }
+//    @Test
+//    public void test_create_location_with_standard_address() throws Exception{
+//        location = new Location(
+//                "Bij Koning Filip", // name
+//                50.885933, // latitude
+//                4.354272, // longitude
+//                "Avenue du Parc Royal 1", // address
+//                "", // cross street
+//                "Laeken", // city
+//                1020, // zip code
+//                "Belgium" // coountry
+//        );
+//    }
+//
+//    @Test( expected = DomainException.class )
+//    public void test_fail_create_location_with_non_existent_coordinates() throws Exception{
+//        location = new Location(
+//                "Bij Koning Filip", // name
+//                150.885933, // latitude
+//                404.354272, // longitude
+//                "Avenue du Parc Royal 1", // address
+//                "", // cross street
+//                "Laeken", // city
+//                1020, // zip code
+//                "Belgium" // coountry
+//        );
+//    }
 }
